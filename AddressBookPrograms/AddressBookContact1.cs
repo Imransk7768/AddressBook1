2e38d0cdd5074d60d5d6cb79dca2e33c4106ff91
Imransk7768 <imransk7768@gmail.com>
2022-04-20 01:52:44 +0530

    [Imran] Added: Contacts Entering in AddressBook Implemented

---
+++ AddressBookPrograms/AddressBookContact1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookPrograms
+{
+    public class AddressBookContact1
+    {
+        List<Contact> addressBook = new List<Contact>();
+        public AddressBookContact1()
+        {
+            Contact address1 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                Mobile = Convert.ToInt64(Console.ReadLine()),
+                PostalCode = Convert.ToInt32(Console.ReadLine()),
+            };
+            Contact address2 = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                Mobile = Convert.ToInt64(Console.ReadLine()),
+                PostalCode = Convert.ToInt32(Console.ReadLine()),
+            };
+            addressBook.Add(address1);
+            addressBook.Add(address2);
+        }
+        public void AddContactToAddressBook(Contact con)
+        {
+            addressBook.Add(con);
+        }
+        public void Display()
+        {
+            foreach (var con in addressBook)
+            {
+                Console.WriteLine("FirstName : " + con.FirstName + "\nLastName : " + con.LastName + 
+                    "\nAddress : " + con.Address +"\nState : " + con.State + "\nEMail : " + con.Email + 
+                    "\nMobile : " + con.Mobile + "\nZIPCode : " + con.PostalCode + ".");
+            }
+        }
+    }
+}
--- AddressBookPrograms/Program.cs
+++ AddressBookPrograms/Program.cs
@@ -12,17 +12,8 @@ namespace AddressBookPrograms
         {
             Console.WriteLine(">>>> Welcome to Address-Book Details <<<<\n");
             Console.WriteLine("Enter Contact Details");
-            Contact con = new Contact();
-            con.FirstName=Console.ReadLine();
-            con.LastName=Console.ReadLine();
-            con.Address=Console.ReadLine();
-            con.State=Console.ReadLine();
-            con.Email=Console.ReadLine();
-            con.Mobile=Convert.ToInt64(Console.ReadLine());
-            con.PostalCode= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("FirstName : "+con.FirstName+ "\nLastName : " + con.LastName + "\nAddress : " + con.Address + 
-                "\nState : " + con.State + "\nEMail : " + con.Email + "\nMobile : " + con.Mobile + "\nZIPCode : " + con.PostalCode +".");
-
+            AddressBookContact1 ad = new AddressBookContact1();
+            ad.Display();
         }
     }
 }
